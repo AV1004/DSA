74ef041cab9a83a1dd53697972a78b8aa00d2f26
Valay Andhariya <andhariyavalay4@gmail.com>
2024-07-08 23:26:59 +0530

    Fix some code

--- practice/practice.js
+++ practice/practice.js
@@ -1,11 +1,24 @@
-function missingNumber(n, arr) {
-  let s = (n * (n + 1)) / 2; // Calculate the sum of first n natural numbers
+// function missingNumber(n, arr) {
+//   let s = (n * (n + 1)) / 2; // Calculate the sum of first n natural numbers
 
-  let sum = 0; // Calculate the sum of elements in the array
+//   let sum = 0; // Calculate the sum of elements in the array
 
-  for (let ar of arr) {
-    sum += ar;
-  } // Return the missing number
+//   for (let ar of arr) {
+//     sum += ar;
+//   } // Return the missing number
 
-  return s - sum;
-}
+//   return s - sum;
+// }
+
+let a = [1, 2, 3, 2, 5, 2],
+  arr = [];
+
+let b = [2];
+
+a.forEach((e) => {
+  if (e !== b[0]) {
+    arr.push(e);
+  }
+});
+
+console.log(arr);
