9b5628f6992fd8da3cc1b580a28310a0416aab7e
Valay Andhariya <andhariyavalay4@gmail.com>
2024-07-08 18:25:41 +0530

    Implementing Bubble Sort

--- 7_Sortings/selectionSort.cpp
+++ 7_Sortings/selectionSort.cpp
@@ -19,6 +19,22 @@ void selection_sort(int arr[], int n)
     }
 }
 
+void bubble_sort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 1; i--)
+    {
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
 int main()
 {
     int n;
@@ -28,10 +44,13 @@ int main()
     {
         cin >> arr[i];
     }
-    selection_sort(arr, n);
+    // selection_sort(arr, n);
+    bubble_sort(arr, n);
+
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
+
     return 0;
 }
