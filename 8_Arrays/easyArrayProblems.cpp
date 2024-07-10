ec077eec87d491c94fc6736906e641fc848f5662
Valay Andhariya <andhariyavalay4@gmail.com>
2024-07-10 18:40:42 +0530

    Implementing Second Largest of Array

--- 8_Arrays/easyArrayProblems.cpp
+++ 8_Arrays/easyArrayProblems.cpp
@@ -14,6 +14,28 @@ int maxELement(int arr[], int n)
     return max;
 }
 
+int secondLargest(int arr[], int n)
+{
+    int largest = arr[0];
+    // int sLargest = -1; // in case of array does not containing minus values
+    int sLargest = INT_MIN; // in case of array containing minus values
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > largest)
+        {
+            sLargest = largest;
+            largest = arr[i];
+        }
+        else if (arr[i] < largest && arr[i] > sLargest)
+        {
+            sLargest = arr[i];
+        }
+    }
+
+    return sLargest;
+}
+
 int main()
 {
     int n;
@@ -23,7 +45,9 @@ int main()
     {
         cin >> arr[i];
     }
-    int ans = maxELement(arr, n);
+    // int ans = maxELement(arr, n);
+    int ans = secondLargest(arr, n);
+    // int ans = secondSmallest(arr, n);
     cout << "Answer is " << ans;
     return 0;
 }
