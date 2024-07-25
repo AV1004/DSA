17d8e3c6f81d88f3eb2fd89b814cf516c38c8cc6
Valay Andhariya <andhariyavalay4@gmail.com>
2024-07-26 04:44:19 +0530

    Solving missing number from array problem

--- 8_Arrays/easyArrayProblems.cpp
+++ 8_Arrays/easyArrayProblems.cpp
@@ -232,6 +232,20 @@ vector<int> intersectionOfArray(vector<int> arr1, int n, vector<int> arr2, int m
 
     return ans;
 }
+
+int missingNumber(vector<int> &a, int N)
+{
+    int xor1 = 0, xor2 = 0;
+    int n = N - 1;
+    for (int i = 0; i < n; i++)
+    {
+        xor2 = xor2 ^ a[i];
+        xor1 = xor1 ^ (i + 1);
+    }
+    xo1 = xor1 ^ N;
+    return xor1 ^ xor2;
+}
+
 int main()
 {
     int n;
